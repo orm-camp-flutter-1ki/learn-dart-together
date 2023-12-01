6469e918a42f7ec3cb72d23fb6d0115a814e21aa
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-01 11:24:00 +0900

    연습문제 1-4 완료

--- lib/23_11_30/cleric.dart
+++ lib/23_11_30/cleric.dart
@@ -2,6 +2,8 @@
 // 연습 1-1에서 작성한 내용이 없는 Cleric 클래스에 “이름", “HP”, “최대 HP”, “MP”, “최대 MP”를 속성으로 추가 하시오.
 // 또한 HP와 최대 HP는 정수로 초기치 50, MP와 최대 MP는 정수로 초기치 10으로 하고,
 // 최대 HP와 최대 MP는 상수 필드로 선언 하시오.
+import 'dart:math';
+
 class Cleric {
   final int maxHp = 50;
   final int maxMp = 10;
@@ -21,5 +23,26 @@ class Cleric {
 
     mp = mp - 5; // mp -= 5
     hp = maxHp;
+  }
+
+  // 성직자는 “기도하기" (pray) 라는 행동을 취할 수 있고, 자신의 MP를 회복한다.
+  // 이 메소드는 인수에 “기도할 시간(초)"를 지정할 수 있고, 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
+
+  int pray(int sec) {
+    // 나중에 계산하려고 미리 값을 저장해 둔다
+    final beforeMp = mp;
+
+    // 회복량은 기도한 시간(초)에 랜덤하게 0 ~ 2포인트의 보정을 한 양이다 (3초 기도하면 회복량은 3 ~ 5 포인트).
+    final recoveryMp = sec + Random().nextInt(3);
+
+    mp += recoveryMp;
+
+    // 단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
+    if (mp > maxMp) {
+      mp = maxMp;
+    }
+
+    // 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
+    return beforeMp - mp;
   }
 }
