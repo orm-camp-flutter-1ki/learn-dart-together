d0f47e10f754a110369bb987669b0d7878e10c0b
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-15 09:33:01 +0900

    Todo

--- lib/23_12_14/01_http2.dart
+++ lib/23_12_14/01_http2.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'todo.dart';
 
 void main() async {
-
   // 실습 1
   // final Todo todo = await getTodo2(1);
   // print(todo.title);
@@ -16,28 +15,28 @@ void main() async {
   todos.forEach((e) {
     print('${e.toJson()}');
   });
-
-
 }
 
 // 여러개 리스트 받을 때 패턴
 Future<List<Todo>> getTodos2() async {
-  // 요청
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
   final jsonList = jsonDecode(response.body) as List<dynamic>;
   return jsonList.map((e) => Todo.fromJson(e)).toList();
 }
 
 // 하나 받을 때 패턴
 Future<Todo> getTodo2(int id) async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
   return Todo.fromJson(jsonDecode(response.body));
 }
 
 // 여러개 리스트 받을 때 패턴
 Future<List<Todo>> getTodos() async {
   // 요청
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
   // jsonString
   final jsonString = response.body;
   // to Map
@@ -47,7 +46,8 @@ Future<List<Todo>> getTodos() async {
 
 Future<Todo> getTodo(int id) async {
   // 요청
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/$id'));
   // jsonString
   final jsonString = response.body;
   // to Map
@@ -57,5 +57,3 @@ Future<Todo> getTodo(int id) async {
 
   return Todo.fromJson(jsonDecode(response.body));
 }
-
-
